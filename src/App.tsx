b67a9ce9bb3473ee5529ca83fda1d3739534d475
anyjava <sht21c@gmail.com>
2022-01-01 05:06:47 +0900

    fix eslint

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,5 @@
-import React, { useState } from 'react';
-
+import React from 'react';
 import './App.css';
-import { Button } from 'antd-mobile';
-import { TabBar } from 'antd-mobile';
-import logo from './logo.svg';
-
 import FeedList from './pages/FeedList';
 
 function App() {
